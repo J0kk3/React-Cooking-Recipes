5143449d4a4bab052c96d6162663b4092c30797c
J0kk3 <joakim_88@msn.com>
2022-06-24 23:39:44 +0200

    [FIX] react-router-dom to v6^

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 //Page Components
 import Home from "./pages/home/Home"
-import Create from './pages/create/create';
-import Search from './pages/search/search';
-import Recipes from "./pages/recipe/recipes";
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipes from "./pages/recipe/Recipes";
 
 //Styles
 import './App.css'
@@ -14,20 +14,12 @@ function App ()
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="search">
-            <Search />
-          </Route>
-          <Route path="/recipes/:id">
-            <Recipes />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/create" element={ <Create /> } />
+          <Route path="search" element={ <Search /> } />
+          <Route path="/recipes/:id" element={ <Recipes /> } />
+        </Routes>
       </BrowserRouter >
     </div >
   );
