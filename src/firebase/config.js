7d4c0e04ff21c8c2cddf8c2168e94ddb6a9c34f2
J0kk3 <joakim_88@msn.com>
2022-06-27 09:43:02 +0200

    [ADD] firebase init

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig =
+{
+    apiKey: "AIzaSyCCtLJMMN0YZlVSefcAMrq3YUrb-0eMjjc",
+    authDomain: "cooking-ninja-site-e7c79.firebaseapp.com",
+    projectId: "cooking-ninja-site-e7c79",
+    storageBucket: "cooking-ninja-site-e7c79.appspot.com",
+    messagingSenderId: "208547179281",
+    appId: "1:208547179281:web:145c37f5c324b4f1ddbab0"
+};
+
+//Init Firebase
+firebase.initializeApp( firebaseConfig );
+
+//Init Services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
